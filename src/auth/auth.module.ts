b23b5363176cd083d9d57bc54df4a605676bcc5f
DardanB <>
2023-07-09 19:01:26 +0200

    error fixed for circular module imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,13 +2,9 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
-import { UserModule } from '../user/user.module';
-import { TransactionModule } from 'src/transaction/transaction.module';
 
 @Module({
   imports: [
-    UserModule,
-    TransactionModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: 'your-secret-key', // replace with your actual secret key
@@ -16,5 +12,6 @@ import { TransactionModule } from 'src/transaction/transaction.module';
     }),
   ],
   providers: [JwtStrategy],
+  exports: [JwtModule, PassportModule, JwtStrategy],  // Export JwtModule and JwtStrategy if they are used in other modules
 })
 export class AuthModule { }
