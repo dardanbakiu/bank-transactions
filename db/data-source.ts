0092617b4c922e76f1943e890db8da77437515ff
DardanB <>
2023-07-09 19:53:02 +0200

    env.example

--- db/data-source.ts
+++ db/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'root',
-  database: 'bank',
+  host: process.env.HOST || 'localhost',
+  port: Number(process.env.PORT) || 3306,
+  username: process.env.USERNAME || 'root',
+  password: process.env.PASSWORD || 'root',
+  database: process.env.DATABASE || 'bank',
   // entities: ['dist/**/*{.ts,.js}'],
   entities: ['dist/src/**/*{.ts,.js}'],
   migrations: ['dist/db/migrations/*{.ts,.js}'],
