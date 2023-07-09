79eea28f208540403fb6caa7016a978fbb689e87
DardanB <>
2023-07-09 11:06:14 +0200

    ex lint ""

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,4 +1,4 @@
-import { DataSource, DataSourceOptions } from "typeorm";
+import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
